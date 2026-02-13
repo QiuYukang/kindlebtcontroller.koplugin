be99f0300c53202a5130e328f37e3f9521cf2415
qiuyukang <b612n@qq.com>
2026-02-13 21:01:40 +0800

    chore(version): Bump version to v0.1.2

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = _("Kindle Bluetooth Controller"),
     description = _("Use a Bluetooth controller to control KOReader on kindle."),
     author = "b612n@qq.com",
-    version = "v0.1.1",
+    version = "v0.1.2",
 }
